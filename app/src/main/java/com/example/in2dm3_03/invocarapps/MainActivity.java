dbe1d9a74fcf36b784fffcabc4daf1e4d8df6412
Loren Aparicio <loren.djkiller@gmail.com>
2017-11-28 12:41:56 +0100

    Initial commit

---
+++ app/src/main/java/com/example/in2dm3_03/invocarapps/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.in2dm3_03.invocarapps;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @SuppressLint("MissingPermission")
+    public void boton(View v) {
+        Button b = (Button) v;
+
+        if (b.getId() == R.id.buttonWeb) {
+            Intent i1 = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.es"));
+            startActivity(i1);
+        }
+        if (b.getId() == R.id.buttonLlamar) {
+            Intent i1 = new Intent(Intent.ACTION_CALL);
+            i1.setData(Uri.parse("tel:123456789"));
+
+            startActivity(i1);
+        }
+
+        if (b.getId()==R.id.buttonLlamada){
+            Intent i1=new Intent(Intent.ACTION_DIAL);
+            startActivity(i1);
+        }
+        if (b.getId()==R.id.buttonMarcar){
+            Intent i1=new Intent(Intent.ACTION_DIAL, Uri.parse("tel:+722466004"));
+            startActivity(i1);
+        }
+    }
+}
